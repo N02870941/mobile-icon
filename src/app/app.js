72b04091169955cef82d6db7a2c7584f87f8defd
Jabari Dash <jabari.dash@gmail.com>
2019-03-17 16:45:27 -0500

    added middleware for error

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,12 @@
 const express  = require('express');
 const app      = express();
 const path     = require('path');
-const template = path.join(__dirname, 'template');
 const multer   = require('multer');
 const util     = require('util');
 const service  = require('./service');
 const storage  = require('./storage');
 const commons  = require('./commons')
+const template = path.join(__dirname, 'template');
 const body_parser = require('body-parser')
 const upload   = util.promisify(multer(storage).single('file'));
 
@@ -18,49 +18,73 @@ app.use(body_parser.json())
 app.use(express.json())
 app.use(express.static(template))
 app.post('/upload', ingress)
+app.use(log_error)
+app.use(handle_client_error)
+app.use(handle_internal_error)
 
 //------------------------------------------------------------------------------
 
 function ingress(req, res, next) {
   upload(req, res)
-  .then(() => edit_icon(req, res))
-  .catch((error) => upload_failed(error, res))
+  .then(() => edit_icon(req, res, next))
+  .catch(next)
 }
 
 //------------------------------------------------------------------------------
 
-function edit_icon(req, res) {
+function edit_icon(req, res, next) {
   const filename = req.body.filename ? `${req.body.filename}.zip` : 'icon.zip'
 
-  service.edit(req.file).then(zip => {
+  service
+  .edit(req.file)
+  .then(zip => {
 
     res.download(zip, filename, error => {
       if (error)
         service.dispatcher.emit('error', error)
 
       service.dispatcher.emit('cleanup')
-    });
+    })
+  })
+  .catch(error => {
+    next(error)
   })
 }
 
 //------------------------------------------------------------------------------
 
-function upload_failed(error, res) {
+function log_error(error, req, res, next) {
   service.dispatcher.emit('error', error)
   service.dispatcher.emit('cleanup')
 
-  const custom_response = {
-    message : error.message,
-    code: 406
-  }
+  console.error(error)
+  next(error)
+}
 
-  const generic_response = {
+//------------------------------------------------------------------------------
+
+function handle_client_error(error, req, res, next) {
+  if (error instanceof commons.CustomError) {
+    const body = {
+      message : error.message,
+      code: 406
+    }
+
+    res.status(body.code).json(body)
+
+  } else {
+    next(error)
+  }
+}
+
+//------------------------------------------------------------------------------
+
+function handle_internal_error(error, req, res, next) {
+  const body = {
     message : 'An unknown error occured. Please report bug',
     url     : 'https://github.com/N02870941/mobile-icon/issues',
     code    : 500
   }
-
-  const body = error instanceof commons.CustomError ? custom_response : generic_response
 
   res.status(body.code).json(body)
 }
